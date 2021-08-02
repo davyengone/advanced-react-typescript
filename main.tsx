ab1e8eb1f8bb8e534502ea2c376282aebd4e64d4
Davy Engone <davy@hackages.io>
2021-08-02 14:22:08 +0200

    fixed main

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import 'bulma/css/bulma.css'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {App} from './advanced-patterns/hoc/01-basic-hoc/app'
+import {App} from './advanced-react/advanced-patterns/hoc/01-basic-hoc/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
